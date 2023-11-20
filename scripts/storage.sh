5ea0c2a0a417168e33ee3e2528242a470f49241f
ステラ <withervt@outlook.com>
2023-11-20 13:28:48 -0500

    feat(Scripts): update storage with more information

--- scripts/storage.sh
+++ scripts/storage.sh
@@ -6,26 +6,32 @@ convert_space() {
     local space_in_gb
     local space_in_mb
 
+    # Check if the space value has 'G' (indicating gigabytes)
     if [[ $space_value == *G ]]; then
         space_in_gb=$(echo $space_value | sed 's/G//')
-        echo "${space_in_gb}GB"
+        echo "${space_in_gb} gigabytes"
     else
+        # If not in gigabytes, assume it's in megabytes
         space_in_mb=$(echo $space_value | sed 's/M//')
-        echo "${space_in_mb}MB"
+        echo "${space_in_mb} megabytes"
     fi
 }
 
-# Get used and free space on the root filesystem
-USED_SPACE=$(df -h / | awk 'NR==2 {print $3}')
-FREE_SPACE=$(df -h / | awk 'NR==2 {print $4}')
-TOTAL_SPACE=$(df -h / | awk 'NR==2 {print $2}')
+# Get disk space information for the root filesystem
+disk_space_info=$(df -h / | awk 'NR==2 {print $2,$3,$4}')
 
-# Convert used, free, and total space to GB or MB
+# Extracting values for total, used, and free space
+read -r TOTAL_SPACE USED_SPACE FREE_SPACE <<< "$disk_space_info"
+
+# Convert used, free, and total space to GB or MB using the convert_space function
 USED=$(convert_space "$USED_SPACE")
 FREE=$(convert_space "$FREE_SPACE")
 TOTAL=$(convert_space "$TOTAL_SPACE")
 
-# Print the results
-echo "Used: $USED"
-echo "Free: $FREE"
-echo "Total: $TOTAL"
+# Print informative results
+echo "Disk Space Information for the Root Filesystem:"
+echo "---------------------------------------------"
+echo "Total Space: $TOTAL"
+echo "Used Space:  $USED"
+echo "Free Space:  $FREE"
+echo "---------------------------------------------"
